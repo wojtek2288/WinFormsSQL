9ce9b75b3d3d88c7a4aad84328b96011393beb24
wojtek2288 <54373359+wojtek2288@users.noreply.github.com>
2021-05-31 16:33:57 +0200

    Add files via upload

---
+++ PD2_Bazy_danych/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace PD1_Bazy_danych
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // dateTimePicker2
+            // 
+            resources.ApplyResources(this.dateTimePicker2, "dateTimePicker2");
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Value = new System.DateTime(1900, 1, 1, 18, 0, 0, 0);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowValidated);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ PD2_Bazy_danych/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PD1_Bazy_danych
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-6170UO7;Initial Catalog=PD1Movies;Integrated Security=True");
+        int index = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetRecords();
+        }
+
+        private void GetRecords()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Movies", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].Width = 162;
+            dataGridView1.Columns[3].DefaultCellStyle.Format = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = SearchCommand();
+            DataTable dt = new DataTable();
+
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private SqlCommand SearchCommand()
+        {
+            var cmd = new SqlCommand
+            {
+                Connection = con
+            };
+
+            var str = new StringBuilder("SELECT * FROM Movies WHERE ReleaseDate >= @startdate AND ReleaseDate <= @enddate AND Imax3D = @imax");
+
+            var sdate = new SqlParameter
+            {
+                ParameterName = "@startdate",
+                Value = dateTimePicker1.Value,
+                SqlDbType = SqlDbType.Date
+            };
+
+            var edate = new SqlParameter
+            {
+                ParameterName = "@enddate",
+                Value = dateTimePicker2.Value,
+                SqlDbType = SqlDbType.Date
+            };
+
+            var imax = new SqlParameter
+            {
+                ParameterName = "@imax",
+                Value = checkBox1.Checked,
+                SqlDbType = SqlDbType.Bit
+            };
+
+            cmd.Parameters.Add(sdate);
+            cmd.Parameters.Add(edate);
+            cmd.Parameters.Add(imax);
+            
+            if(textBox1.Text.Length != 0)
+            {
+                str.Append(" AND Title LIKE @title");
+                var title = new SqlParameter
+                {
+                    ParameterName = "@title",
+                    Value = $"%{textBox1.Text}%",
+                    SqlDbType = SqlDbType.VarChar,
+                    Size = 50
+                };
+                cmd.Parameters.Add(title);
+            }
+            
+            if(textBox2.Text.Length != 0)
+            {
+                str.Append(" AND Budget >= @budgetmin");
+                var budgetmin = new SqlParameter
+                {
+                    ParameterName = "@budgetmin",
+                    Value = int.Parse(textBox2.Text),
+                    SqlDbType = SqlDbType.Money,
+                };
+                cmd.Parameters.Add(budgetmin);
+            }
+
+            if (textBox3.Text.Length != 0)
+            {
+                str.Append(" AND Budget <= @budgetmax");
+                var budgetmax = new SqlParameter
+                {
+                    ParameterName = "@budgetmax",
+                    Value = int.Parse(textBox3.Text),
+                    SqlDbType = SqlDbType.Money,
+                };
+                cmd.Parameters.Add(budgetmax);
+            }
+
+            if(textBox4.Text.Length != 0)
+            {
+                str.Append(" AND AvgRating >= @avgminrating");
+                var avgminrating = new SqlParameter
+                {
+                    ParameterName = "@avgminrating",
+                    Value = double.Parse(textBox4.Text),
+                    SqlDbType = SqlDbType.Float,
+                    Scale = 18,
+                    Precision = 2
+                };
+                cmd.Parameters.Add(avgminrating);
+            }
+
+            if (textBox5.Text.Length != 0)
+            {
+                str.Append(" AND AvgRating <= @avgmaxrating");
+                var avgmaxrating = new SqlParameter
+                {
+                    ParameterName = "@avgmaxrating",
+                    Value = double.Parse(textBox5.Text),
+                    SqlDbType = SqlDbType.Float,
+                    Scale = 18,
+                    Precision = 2
+                };
+                cmd.Parameters.Add(avgmaxrating);
+            }
+
+            cmd.CommandText = str.ToString();
+            return cmd;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Movies SET " + dataGridView1.Columns[e.ColumnIndex].Name + " = @cell WHERE Id = " + dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString(), con);
+            switch(dataGridView1.Columns[e.ColumnIndex].Name)
+            {
+                case "Title":
+                    var cell1 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString(),
+                        SqlDbType = SqlDbType.VarChar,
+                        Size = 50
+                    };
+                    cmd.Parameters.Add(cell1);
+                    break;
+                case "ReleaseDate":
+                    var cell2 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = (DateTime)dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value,
+                        SqlDbType = SqlDbType.Date
+                    };
+                    cmd.Parameters.Add(cell2);
+                    break;
+                case "Budget":
+                    var cell3 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()),
+                        SqlDbType = SqlDbType.Money
+                    };
+                    cmd.Parameters.Add(cell3);
+                    break;
+                case "AvgRating":
+                    var cell4 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = double.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()),
+                        SqlDbType = SqlDbType.Float,
+                        Scale = 18,
+                        Precision = 2
+                    };
+                    cmd.Parameters.Add(cell4);
+                    break;
+                case "Imax3D":
+                    var cell5 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = bool.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()),
+                        SqlDbType = SqlDbType.Bit
+                    };
+                    cmd.Parameters.Add(cell5);
+                    break;
+                case "TicketsSold":
+                    var cell6 = new SqlParameter
+                    {
+                        ParameterName = "@cell",
+                        Value = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString()),
+                        SqlDbType = SqlDbType.Int
+                    };
+                    cmd.Parameters.Add(cell6);
+                    break;
+            }
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        private void AddRow()
+        {
+            if (index != -1)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Movies VALUES (@title, @date, @budget, @avgrating, @imax, @tickets)", con);
+                var title = new SqlParameter
+                {
+                    ParameterName = "@title",
+                    Value = dataGridView1.Rows[index].Cells[1].Value.ToString(),
+                    SqlDbType = SqlDbType.VarChar,
+                    Size = 50
+                };
+                var date = new SqlParameter
+                {
+                    ParameterName = "@date",
+                    Value = (DateTime)dataGridView1.Rows[index].Cells[2].Value,
+                    SqlDbType = SqlDbType.Date
+                };
+                var budget = new SqlParameter
+                {
+                    ParameterName = "@budget",
+                    Value = Int32.Parse(dataGridView1.Rows[index].Cells[3].Value.ToString()),
+                    SqlDbType = SqlDbType.Money
+                };
+                var avgrating = new SqlParameter
+                {
+                    ParameterName = "@avgrating",
+                    Value = double.Parse(dataGridView1.Rows[index].Cells[4].Value.ToString()),
+                    SqlDbType = SqlDbType.Float,
+                    Scale = 18,
+                    Precision = 2
+                };
+                var imax = new SqlParameter();
+                imax.ParameterName = "@imax";
+                imax.SqlDbType = SqlDbType.Bit;
+                if (dataGridView1.Rows[index].Cells[5].Value != null)
+                {
+                    imax.Value = bool.Parse(dataGridView1.Rows[index].Cells[5].Value.ToString());
+                }
+                else
+                {
+                    imax.Value = false;
+                }
+                    
+                var tickets = new SqlParameter
+                {
+                    ParameterName = "@tickets",
+                    Value = Int32.Parse(dataGridView1.Rows[index].Cells[6].Value.ToString()),
+                    SqlDbType = SqlDbType.Int
+                };
+
+                cmd.Parameters.Add(title);
+                cmd.Parameters.Add(date);
+                cmd.Parameters.Add(budget);
+                cmd.Parameters.Add(avgrating);
+                cmd.Parameters.Add(imax);
+                cmd.Parameters.Add(tickets);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            index = -1;
+        }
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            index = e.Row.Index - 1;
+        }
+
+        private void dataGridView1_RowValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            AddRow();
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM Movies WHERE Id =" + dataGridView1.Rows[e.Row.Index].Cells[0].Value.ToString(), con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
